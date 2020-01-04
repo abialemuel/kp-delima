6fc39c9466d8041ea4481fb8e869acf84a0c5197
abialemuel <abia.darma@bukalapak.com>
2020-01-04 23:36:43 +0700

    update index

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php 
-    require_once __DIR__.'/../vendor/autoload.php';
-    $app = new Silex\Application();
-    $app->post('/webhook', function (Request $request) {
-        header('Content-Type: application/json');
-        $request = file_get_contents('php://input');
-        $req_dump = print_r( $request, true );
-        $fp = file_put_contents( 'webhook_request.log', $req_dump );
-    });
-    $app->run();
-?>
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+  return 'Hello '.$app->escape($name);
+});
+
+$app->run();
