362cf6abb0113a466e82c3c6b619c61c8c1e7271
abialemuel <abia.darma@bukalapak.com>
2020-01-05 15:35:47 +0700

    helper

--- helper.php
+++ helper.php
@@ -7,6 +7,6 @@
     {
         $request = file_get_contents('php://input');
         $req_dump = print_r( $request, true );
-        $fp = file_put_contents( 'webhook_request.log', $req_dump );
+        $fp = file_put_contents( 'webhook_request.log', $req_dump, FILE_APPEND );
     }
 ?>
